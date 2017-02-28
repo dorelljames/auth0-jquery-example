cbe055cdf7c9405930b9b0ad5d3835ebc5df749e
Dorell James Galang <galangdj@gmail.com>
2017-02-28 17:27:58 +0800

    Updates

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
   var route = function() {
     var id_token = localStorage.getItem('id_token');
     var current_location = window.location.pathname;
-    $(".container").show();
+
     if (undefined != id_token) {
       var profile = JSON.parse(localStorage.getItem('profile'));
 
@@ -62,6 +62,7 @@ $(document).ready(function() {
           $('#btn-logout').show();
           $('#btn-go-admin').show();
           $('#btn-go-user').show();
+          $(".container").show();
           break;
         case "/user" || "/user.html":
           if (true != isUser(profile)) {
